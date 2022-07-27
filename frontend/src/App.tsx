a1d41c9f807b8957a765ccb31528ee2b5a179a93
Cristiano Pontes <pontescr@gmail.com>
2022-07-27 16:57:01 -0300

    Created components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import Header from "./components/Header";
+import NotificationIcon from "./components/NotificationIcon";
+import SalesCard from "./components/SalesCard";
+
 function App() {
   return (
-    <h1>Olá Mundo</h1>
-  )
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/NotificationIcon/index.tsx
@@ -0,0 +1,13 @@
+import Icon from "../../assets/imgs/Notification.svg";
+
+import "./styles.css";
+
+function NotificationIcon() {
+  return (
+    <div className="dsmeta-red-btn">
+      <img src={Icon} alt="Notificar" />
+    </div>
+  );
+}
+
+export default NotificationIcon;
