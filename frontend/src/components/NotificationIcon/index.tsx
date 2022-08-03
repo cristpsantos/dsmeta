1c3c9b6a04a52781733bce2c174c3ac394303068
Cristiano Pontes <pontescr@gmail.com>
2022-08-02 21:45:55 -0300

    Send Message Notification

--- frontend/src/components/NotificationIcon/index.tsx
+++ frontend/src/components/NotificationIcon/index.tsx
@@ -1,10 +1,22 @@
+import axios from "axios";
 import Icon from "../../assets/imgs/Notification.svg";
+import { BASE_URL } from "../../util/requests";
 
 import "./styles.css";
 
-function NotificationIcon() {
+type Props = {
+  saleId: number;
+};
+
+function handleClick(id: number) {
+  axios.get(`${BASE_URL}/sales/${id}/notification`).then(() => {
+    console.log("Enviada com sucesso!");
+  });
+}
+
+function NotificationIcon({ saleId }: Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={Icon} alt="Notificar" />
     </div>
   );
